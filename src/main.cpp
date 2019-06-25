f1b6490544758964e2240143b0d39d1cbeb0a38c
Jonathon Greene <jongreene@ku.edu>
2019-06-25 12:33:47 -0500

    setup leuart echoback

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include "mbed.h"
 #include "PlatformInterface.h"
 #include "Commands.h"
+#include "LUARTHandler.h"
 
 //// Required to fix link error inside arm mbed-os
 // fpv4-sp/softfp/libc.a(lib_a-signalr.o): In function `_getpid_r'
@@ -13,10 +14,18 @@ int _kill(){return 0;}
 DigitalOut bcEnablePin(PF7);
 
 int main() {
-    //Disable the board controller serial connection
-    bcEnablePin = 0;
+//    //Disable the board controller serial connection
+//    bcEnablePin = 0;
+//
+//	auto communicator = new PlatformInterface();
 
-	auto communicator = new PlatformInterface();
+	leuartInit();
+
+	while (1)
+	{
+		/* On every wakeup enter EM2 again */
+		EMU_EnterEM2(true);
+	}
 
 	return 0;
 }
